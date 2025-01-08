ecc8fced42c6d8a70fed15c3786efdb32327f79b
wangdingyun <2687158269@qq.com>
2025-01-08 15:12:28 +0800

    2

--- src/app/pages/login/page.tsx
+++ src/app/pages/login/page.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { loginApi } from "@/api/auth";
 import Link from "next/link";
+import DynamicBackground from "@/components/DynamicBackground ";
+import { motion } from "framer-motion";
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -12,8 +14,7 @@ export default function Login() {
   ) {
     e.preventDefault();
     await loginApi(username,password).then((res)=>{
-      if(res.status === 200){
-        alert("登录成功");
+      if(res.status === 200){                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
         if (window.location.pathname !== '/') {
           window.location.replace('/');
         }
@@ -26,46 +27,78 @@ export default function Login() {
     })
   };
   return (
-    <div className="flex justify-center items-center h-screen bg-slate-400 ">
-      <form
-        onSubmit={handleLoginSubmit}
-        className="flex flex-col bg-slate-300 rounded-2xl px-8 py-4 space-y-4 "
+    <div className="min-h-screen flex items-center justify-center  overflow-hidden bg-blue-300">
+      <DynamicBackground  />
+      <motion.div className="flex flex-col bg-slate-300 rounded-2xl z-10 "
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
       >
-        <h2 className="mx-auto">登录</h2>
-        <input
-          type="text"
-          placeholder="请输入邮箱"
-          onChange={(e) => setUsername(e.target.value)}
-          name="username"
-          required
-          className=" h-6 rounded-md bg-white bg-opacity-20 placeholder-black"
-        />
-        <input
-          type="password"
-          placeholder="请输入密码"
-          onChange={(e) => setPassword(e.target.value)}
-          name="password"
-          required
-          className=" h-6 rounded-md bg-white bg-opacity-20 placeholder-black"
-        />
-        <button
-          type="submit"
-          className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-1 px-4 rounded-md transition duration-300 ease-in-out transform hover:scale-105"
+        <motion.h1 
+          className="text-4xl font-bold mb-6 text-center text-white"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
         >
           登录
-        </button>
-        <p className="text-center text-[10px] text-gray-500">
-          还没有账号？
-          <span className="text-blue-500">
-            <Link href="pages/register">立即注册</Link>
-          </span>
-        </p>
-        {/* <p className="text-center text-[10px] text-gray-500">
-          <span className="text-blue-500">
-            <Link href="/pages/forgetPassword">忘记密码?</Link>
-          </span>
-        </p> */}
-      </form>
+        </motion.h1>
+        <form
+          onSubmit={handleLoginSubmit}
+          className="flex flex-col  rounded-2xl px-8 pt-4 pb-4 space-y-4 "
+
+        >
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.3, duration: 0.5 }}
+          >
+
+          <input
+            type="text"
+            placeholder="请输入邮箱"
+            onChange={(e) => setUsername(e.target.value)}
+            name="username"
+            required
+            className=" h-6 rounded-md border-white border-2 bg-white bg-opacity-20 placeholder-black"
+          />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.3, duration: 0.5 }}
+          >
+
+          <input
+            type="password"
+            placeholder="请输入密码"
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            required
+            className=" h-6 rounded-md border-white border-2 bg-white bg-opacity-20 placeholder-black"
+          />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.5, duration: 0.5 }}
+          >
+
+          <button
+            type="submit"
+            className="w-full bg-blue-300 border-white border-2 hover:bg-blue-400 text-white font-bold py-1 px-4 rounded-md transition duration-300 ease-in-out transform hover:scale-105"
+          >
+            登录
+          </button>
+          <p className="text-center text-[10px] text-gray-500 mt-4">
+            还没有账号？
+            <span className="text-blue-500">
+              <Link href="pages/register">立即注册</Link>
+            </span>
+          </p>
+          </motion.div>
+
+        </form>
+      </motion.div>
     </div>
   );
 }
--- src/app/pages/register/page.tsx
+++ src/app/pages/register/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 import { useState } from "react";
-import useLoginStore from "@/app/store/useLoginStore";
 import { registerApi } from "@/api/auth";
 import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
+import DynamicBackground from "@/components/DynamicBackground ";
 export default function Register() {
   const [username,setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const setAuth = useLoginStore((state: any) => state.setAuth);
   const router = useRouter();
   const handleRegisterSubmit = async function handleRegisterSubmit(
     e: React.FormEvent
@@ -26,44 +25,129 @@ export default function Register() {
       }
     })
   };
+  // return (
+  //   <div className="flex justify-center items-center h-screen bg-slate-400 ">
+  //     <form
+  //       onSubmit={handleRegisterSubmit}
+  //       className="flex flex-col bg-slate-300 rounded-2xl px-8 py-4 space-y-4 "
+  //     >
+  //       <h2 className="mx-auto">注册</h2>
+  //       <input
+  //         type="text"
+  //         placeholder="请输入用户名"
+  //         onChange={(e) => setUsername(e.target.value)}
+  //         name="username"
+  //         required
+  //         className=" h-6 rounded-md bg-white bg-opacity-20 placeholder-black"
+  //       />
+  //       <input
+  //         type="password"
+  //         placeholder="请输入密码"
+  //         onChange={(e) => setPassword(e.target.value)}
+  //         name="password"
+  //         required
+  //         className=" h-6 rounded-md bg-white bg-opacity-20 placeholder-black"
+  //       />
+  //       <input
+  //         type="text"
+  //         placeholder="请输入邮箱"
+  //         onChange={(e) => setEmail(e.target.value)}
+  //         name="email"
+  //         required
+  //         className=" h-6 rounded-md bg-white bg-opacity-20 placeholder-black"
+  //       />
+  //       <button
+  //         type="submit"
+  //         className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-1 px-4 rounded-md transition duration-300 ease-in-out transform hover:scale-105"
+  //       >
+  //         注册
+  //       </button>
+  //     </form>
+  //   </div>
+  // );
   return (
-    <div className="flex justify-center items-center h-screen bg-slate-400 ">
-      <form
-        onSubmit={handleRegisterSubmit}
-        className="flex flex-col bg-slate-300 rounded-2xl px-8 py-4 space-y-4 "
+    <div className="min-h-screen flex items-center justify-center  overflow-hidden bg-blue-300">
+      <DynamicBackground  />
+      <motion.div className="flex flex-col bg-slate-300 rounded-2xl z-10 "
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
       >
-        <h2 className="mx-auto">注册</h2>
-        <input
+        <motion.h1 
+          className="text-4xl font-bold mb-4 text-center text-white"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+        >
+          注册
+        </motion.h1>
+        <form
+          onSubmit={handleRegisterSubmit}
+          className="flex flex-col  rounded-2xl px-8 pt-4 pb-4 space-y-4 "
+
+        >
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.3, duration: 0.5 }}
+          >
+
+          <input
           type="text"
           placeholder="请输入用户名"
           onChange={(e) => setUsername(e.target.value)}
           name="username"
           required
-          className=" h-6 rounded-md bg-white bg-opacity-20 placeholder-black"
+          className=" h-6 rounded-md border-white border-2 bg-white bg-opacity-20 placeholder-black"
         />
-        <input
-          type="password"
-          placeholder="请输入密码"
-          onChange={(e) => setPassword(e.target.value)}
-          name="password"
-          required
-          className=" h-6 rounded-md bg-white bg-opacity-20 placeholder-black"
-        />
-        <input
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.3, duration: 0.5 }}
+          >
+
+          <input
+            type="password"
+            placeholder="请输入密码"
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            required
+            className=" h-6 rounded-md bg-white border-white border-2 bg-opacity-20 placeholder-black"
+          />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ delay: 0.3, duration: 0.5 }}
+          >
+            
+          <input
           type="text"
           placeholder="请输入邮箱"
           onChange={(e) => setEmail(e.target.value)}
           name="email"
           required
-          className=" h-6 rounded-md bg-white bg-opacity-20 placeholder-black"
+          className=" h-6 rounded-md border-white border-2 bg-white bg-opacity-20 placeholder-black"
         />
-        <button
-          type="submit"
-          className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-1 px-4 rounded-md transition duration-300 ease-in-out transform hover:scale-105"
-        >
-          注册
-        </button>
-      </form>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.5, duration: 0.5 }}
+          >
+
+          <button
+            type="submit"
+            className="w-full bg-blue-300 border-white border-2 hover:bg-blue-400 mb-4 text-white font-bold py-1 px-4 rounded-md transition duration-300 ease-in-out transform hover:scale-105"
+          >
+            注册
+          </button>
+
+          </motion.div>
+
+        </form>
+      </motion.div>
     </div>
   );
 }
