6222d54a98af225d55663aef48deac7fc2820c24
dd <2687158269@qq.com>
2025-01-09 23:06:47 +0800

    1.9

--- src/api/auth.tsx
+++ src/api/auth.tsx
@@ -17,30 +17,3 @@ export async function loginApi(username:any,password:any){
         body:JSON.stringify({username,password})
     })
 }
-// export async function getCaptchaApi(username_or_email:any){
-//     console.log(JSON.stringify({username_or_email}));
-//     return fetch("http://127.0.0.1:9005/ai/users/password/reset/code",{
-//         method:"POST",
-//         headers:{
-//             "Content-Type":"application/json"
-//         },
-//         body:JSON.stringify({username_or_email})
-//     })
-// }
-// export async function resetPasswordApi(code:any,password:any,confirmPassword:any){
-//     return fetch("http://127.0.0.1:9005/ai/users/password/reset",{
-//         method:"POST",
-//         headers:{
-//             "Content-Type":"application/json"
-//         },
-//         body:JSON.stringify({code,password,confirmPassword})
-//     })
-// }
-// export async function resetPasswordDown(){
-//     return fetch("http://127.0.0.1:9005/ai/users/password/reset/down",{
-//         method:"GET",
-//         headers:{
-//             "Content-Type":"application/json"
-//         }
-//     })
-// }
--- src/app/pages/forgetPassword/layout.tsx
+++
@@ -1,7 +0,0 @@
-// export default function ForgetPasswordLayout({children}:{children:React.ReactNode}){
-//     return (
-//         <div className="flex justify-center items-center h-screen bg-slate-400">
-//             {children}
-//         </div>
-//     )
-// }
